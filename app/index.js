8ec58026d9f92f2b1cdd4c15d5841963d3a4f29b
Kevin Carr <aestrro@gmail.com>
2016-09-10 14:50:14 -0700

    Development Updates
    * Remove unnecessary production.index.js
    * Update package.json to include electron-prebuilt (previously removed due to testing)

--- app/index.js
+++ app/index.js
@@ -1,3 +1,9 @@
 const path = require('path');
-var appRoot = path.join(__dirname, '..');
+
+if (process.env.NODE_ENV === 'development') {
+  var appRoot = path.join(__dirname, '..');
+} else {
+  var appRoot = path.join(__dirname);
+}
+
 require('electron-compile').init(appRoot, './entry');
