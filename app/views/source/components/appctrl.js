9bb9932de2ceb97950db310ec9f12eaede69cead
Josh Owens <jashcash@gmail.com>
2016-10-23 03:02:55 +0900

    Fixed AppControl buttons

--- app/views/source/components/appctrl.js
+++ app/views/source/components/appctrl.js
@@ -4,9 +4,6 @@ import {
 } from 'react-icons/lib/io';
 
 const AppCtrl = ({onCloseApp, onMaximizeToggle, onMinimizeToggle, ipc}) => {
-  function onCloseApp() {
-    ipc.send('app_close');
-  }
   return (
     <ul className={`AppControl`}>
       <li><a onClick={() => onCloseApp()} href="#"><IoClose/></a></li>
