2c5a681ed5ad157dbaa131f0f2e5136f1498555c
Josh Owens <jashcache@gmail.com>
2016-10-24 01:10:29 +0900

    Fixed index.js

---
+++ app/views/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
+import { syncHistoryWithStore } from 'react-router-redux';
+import configureStore from './main_source/store/configureStore';
+import routes from './main_source/routes';
+
+const store = configureStore();
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
+const history = syncHistoryWithStore(appHistory, store);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+      <Router history={history} routes={routes} />
+    </Provider>
+    , document.getElementById('react-root')
+);
